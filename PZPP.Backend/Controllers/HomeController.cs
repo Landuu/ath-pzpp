255df4817065b6b7d4fb664e632a49ec3b008330
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-04-23 12:37:57 +0200

    Removed unused code

--- PZPP.Backend/Controllers/HomeController.cs
+++ PZPP.Backend/Controllers/HomeController.cs
@@ -10,35 +10,12 @@ namespace PZPP.Backend.Controllers
     [Route("/api")]
     public class HomeController : ControllerBase
     {
-        private readonly JWTSettings settings;
-
-        public HomeController(IConfiguration configuration)
-        {
-            settings = configuration.GetSection("JWT").Get<JWTSettings>()!;
-        }
+        public HomeController() { }
 
         [HttpGet]
         public IResult Get()
         {
             return Results.Ok("git");
-        }
-
-        [HttpGet("authorized")]
-        public async Task<IResult> GetAuthorized()
-        {
-            var th = new JwtSecurityTokenHandler();
-
-            var helper = new JWTHelper(settings);
-            var ar = await th.ValidateTokenAsync(Request.Cookies["token"], helper.GetValidationParameters());
-
-            var u = User.Identity;
-            return Results.Text("authorized");
-        }
-
-        [HttpPost("post")]
-        public async Task<IResult> PostTest([FromForm] string test)
-        {
-            return Results.Ok();
         }
     }
 }
