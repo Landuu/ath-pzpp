7362885021c3a7e4bd09d2e10417ac6359e02281
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-04-30 13:49:57 +0200

    Fix for expired access tokens in user context

--- PZPP.Backend/Handlers/CustomAuthorizationMiddlewareResultHandler.cs
+++ PZPP.Backend/Handlers/CustomAuthorizationMiddlewareResultHandler.cs
@@ -1,16 +1,39 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authorization.Policy;
+using PZPP.Backend.Services.Auth;
 
 namespace PZPP.Backend.Handlers
 {
     public class CustomAuthorizationMiddlewareResultHandler : IAuthorizationMiddlewareResultHandler
     {
         private readonly AuthorizationMiddlewareResultHandler _defeaultHandler = new();
+        private readonly IAuthService _authService;
+
+        public CustomAuthorizationMiddlewareResultHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
 
         public async Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
         {
             if (authorizeResult.Challenged && policy.Requirements.OfType<UserContextRequirement>().Any())
             {
+                string? refreshToken = context.Request.Cookies[_authService.JWTSettings.CookieKeyRefresh];
+                if (refreshToken is not null)
+                {
+                    bool isRefreshValid = await _authService.ValidateRefreshToken(refreshToken);
+                    if(isRefreshValid)
+                    {
+                        // If refresh valid, return 401 and let the frontend handle the refresh
+                        context.Response.StatusCode = 401;
+                        return;
+                    } else
+                    {
+                        // Refresh is invalid - delete cookies if exist and return 204 No Content
+                        _authService.DeleteTokenCookies(context.Response);
+                    }
+                }
+
                 context.Response.StatusCode = 204;
                 return;
             }
